805e79a0c796bbca374e6bd811926789336e11e3
shraddhaa-web <shraddha.bokade@doodlodesigns.com>
2024-07-25 16:54:49 +0530

    fixed header responsive design

--- app/Views/include/sidebar.php
+++ app/Views/include/sidebar.php
@@ -1,3 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <title>Mediresale</title>
+    <meta
+      content="width=device-width, initial-scale=1.0, shrink-to-fit=no"
+      name="viewport"
+    />
+    <link
+      rel="icon"
+      href="<?= base_url('assets/img/logo-small.png'); ?>"
+      type="image/x-icon"
+    />
+
+<!-- google font -->
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Urbanist:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+
+  <!-- Fonts and icons -->
+  <script src="<?= base_url('assets/js/plugin/webfont/webfont.min.js'); ?>"></script>
+    <script>
+      WebFont.load({
+        google: { families: ["Public Sans:300,400,500,600,700"] },
+        custom: {
+          families: [
+            "Font Awesome 5 Solid",
+            "Font Awesome 5 Regular",
+            "Font Awesome 5 Brands",
+            "simple-line-icons",
+          ],
+          urls: ["<?= base_url('assets/css/fonts.min.css'); ?>"],
+        },
+        active: function () {
+          sessionStorage.fonts = true;
+        },
+      });
+    </script>
+
+    <!-- CSS Files -->
+    <link rel="stylesheet" href="<?= base_url('assets/css/bootstrap.min.css'); ?>" />
+    <link rel="stylesheet" href="<?= base_url('assets/css/plugins.min.css'); ?>" />
+    <link rel="stylesheet" href="<?= base_url('assets/css/kaiadmin.min.css'); ?>" />
+    <link rel="stylesheet" href="<?= base_url('assets/css/custom.css'); ?>" />
+
+    <!-- SweetAlert2 CSS -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.min.css">
+
+  </head>
+  <body>
+<div class="wrapper">
  <!-- Sidebar -->
  <div class="sidebar" data-background-color="dark">
         <div class="sidebar-logo">
--- app/Views/layout/layout.php
+++ app/Views/layout/layout.php
@@ -1,6 +1,5 @@
-<?= $this->include('include/header') ?>
 <?= $this->include('include/sidebar') ?>
+<?= $this->include('include/header') ?>
 <!-- Main content -->
 <?= $this->renderSection('content') ?>
-
 <?= $this->include('/include/footer') ?>
