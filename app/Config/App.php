f5395aaff1fad2773de6ec326fd29db9498f58a0
imposglobal <158268388+imposglobal@users.noreply.github.com>
2024-08-27 11:08:55 +0530

    Update App.php

--- app/Config/App.php
+++ app/Config/App.php
@@ -16,7 +16,7 @@ class App extends BaseConfig
      *
      * E.g., http://example.com/
      */
-    public string $baseURL = 'http://localhost/mediresaledash/';
+    public string $baseURL = 'https://app.mediresale.in/';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
